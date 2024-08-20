538448414fc1d793ceafdfc31fbb49a6522e0656
sudiptaMukherjee2001 <frontendDeveloperabbs@gmail.com>
2024-08-20 12:19:54 +0530

    create api for fetch all task

---
+++ backend/src/controller/getAllTask.controller.js
@@ -0,0 +1,17 @@
+import { task_creation } from "../model/taskCreate.model.js"
+
+export const handelGetAllTask = async (req, res) => {
+    try {
+        const allTask = await task_creation.find({})
+        if (!allTask) {
+            return res.status(404).json({ message: "No task found" })
+        }
+
+        res.status(200).json(allTask)
+
+
+    } catch (error) {
+        res.status(400).error({ error: error.message });
+
+    }
+}
--- backend/src/routes/task.routes.js
+++ backend/src/routes/task.routes.js
@@ -3,11 +3,13 @@ import { Router } from "express";
 import createTask from "../controller/CreateTask.Controller.js";
 import { handelUpdateTask } from "../controller/updateTask.controller.js";
 import { handelDeleteAllTask, handelDeleteTaskById } from "../controller/deleteTask.controller.js";
+import { handelGetAllTask } from "../controller/getAllTask.controller.js";
 
 
 
 const router = Router();
-
+/* get all the information */
+router.route("/").get(handelGetAllTask);
 /* Create */
 router.route("/create").post(createTask);
 /* update */
